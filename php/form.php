739167729473af23cdfe4964be891bcd43a680ca
Adiss <adiss.b17@gmail.com>
2014-03-26 17:49:48 +0100

    Űrlap feltöltve

---
+++ php/form.php
@@ -0,0 +1,33 @@
+<script type='text/javascript'>
+    function jatekosADD(){
+        // Játékosok száma
+        var number = document.getElementById("jatekos").value;
+        // Az űrlap, amiben az inputok lesznek
+        var container = document.getElementById("urlap");
+        // Az űrlap esetleg meglévő elemeinek törlése
+        while (container.hasChildNodes()) {
+            container.removeChild(container.lastChild);
+        }
+        for (i=0;i<number;i++){
+            // Input előtti szöveg kiíratása
+            container.appendChild(document.createTextNode((i+1) + ". Játékos"))
+            // Az input elem definiálása
+            var input = document.createElement("input");
+            input.type = "text";
+            input.name = "jatekos" + i;
+            container.appendChild(input);
+            // Sortötés
+            container.appendChild(document.createElement("br"));
+        }
+        // A submit elem létrehozása
+        var submit = document.createElement("input");
+        submit.type = "submit";
+        submit.value = "Kész"
+        container.appendChild(submit);
+    }
+</script>
+<div align="center">
+	Játékosok száma <input type="text" id="jatekos" name="jatekos" value=""><br />
+	<a href="#" onclick="jatekosADD()">Tovább</a>
+	<form id="urlap" name="jatekosok" method="post" action="index.php?oldal=asztal"/>
+</div>
