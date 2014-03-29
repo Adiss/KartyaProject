9d8dc91e874902ea7e3e84795fd5a88a55254e00
Adiss <adiss.b17@gmail.com>
2014-03-29 23:24:20 +0100

    Szabály patch
    Mostmár lehet szabályokat is hozni a királlyal

--- php/asztal.php
+++ php/asztal.php
@@ -1,39 +1,24 @@
 <table align="center" width="100%">
 	<tr>
-		<?php
-			// Játékosok száma
-			$jatekosok = count($_POST);
-			$td_width = 100/$jatekosok;
-			// Játékosok asztalhoz ültetése
-			for($i = 0; $i<$jatekosok; $i++){
-				echo '<th width="'.$td_width.'%">'.$_POST["jatekos".$i.""].'</th>';
-			}
-		?>
+		<th width="25%"><img src="images/cards.png"></th>
+		<th width="25%">Sör</th>
+		<th width="25%">Dáma</th>
+		<th width="25%">Ász</th>
 	</tr>
-	<tr>
-		<?php
-			// Sör
-			for($i = 0; $i<$jatekosok; $i++){
-				echo '<td width="'.$td_width.'%" height="90px" id="jatekos'.$i.'"></td>';
-			}
-		?>
-	</tr>
-	<tr>
-		<?php
-			// Dáma
-			for($i = 0; $i<$jatekosok; $i++){
-				echo '<td width="'.$td_width.'%" height="65px" id="dama'.$i.'" align="center"></td>';
-			}
-		?>
-	</tr>
-	<tr>
-		<?php
-			// Király
-			for($i = 0; $i<$jatekosok; $i++){
-				echo '<td width="'.$td_width.'%" height="65px" id="asz'.$i.'"></td>';
-			}
-		?>
-	</tr>
+	<?php
+		// Játékosok száma
+		$jatekosok = count($_POST);
+		$td_width = 100/$jatekosok;
+		// Játékosok asztalhoz ültetése
+		for($i = 0; $i<$jatekosok; $i++){
+			echo '	<tr>
+						<th width="25%">'.$_POST["jatekos".$i.""].'</th>
+						<td width="25%" height="43px" id="jatekos'.$i.'">
+						<td width="25%" height="43px" id="dama'.$i.'"></td>
+						<td width="25%" height="43px" id="asz'.$i.'"></td>
+					</tr>';
+		}
+	?>
 </table>
 <br>
 <table align="center" width="263px" height="100px">
@@ -44,6 +29,13 @@
 		<td id="kartyahuzas" align="center">
 			<a href="#" onclick="randomkartya(); soros();">Kártya húzás</a>
 		</td>
+	</tr>
+</table>
+<br>
+<table id="szabalyok" align="center" width="263px">
+	<th>Szabályok</th>
+	<tr>
+		<td id="szabaly_textbox">Ha királyt húzol, új szabályt írhatsz be</td>
 	</tr>
 </table>
 <script type="text/javascript" src="js/kartya.js"></script>
--- php/js/kartya.js
+++ php/js/kartya.js
@@ -4,15 +4,15 @@
 
 
 
-var players = document.getElementsByTagName("th").length; 	// Játékosok száma (THEAD elemek megszámolásával kiszámítva)
+var players = (document.getElementsByTagName("th").length)-5; 	// Játékosok száma (THEAD elemek megszámolásával kiszámítva)
 var korok = 0; 												// Körök száma (A "soros" függvény lefutásával növekszik)
 var dama = 0; 												// Van-e valakinél dáma (Ha van akkor az értéke 1, ha nincs akkor 0. Alapértelemezetten nincs senkinél.)
 var asz = 0; 												// Van-e valakinél ász (Ha van akkor az értéke 1, ha nincs akkor 0. Alapértelemezetten nincs senkinél.)
 var n = 0; 													// A soron következő játékos (Értéke (körök mod jatekosok), azaz minden teljes kör után reset 0-ra.)
 var tmp_n_dama = 0;											// Ideiglenes változó (Éppen hanyas játékosnál van a dáma)
 var tmp_n_asz = 0; 											// Ideiglenes változó (Éppen hanyas játékosnál van az ász)
-var tmp_n = 0; 												// Ideiglenes változó (Annak a játékosnak a száma, akinél utoljára volt a sör a pakli elfogyása után)
-
+var tmp_n = 0;												// Ideiglenes változó (Annak a játékosnak a száma, akinél utoljára volt a sör a pakli elfogyása után)
+var szabalyszamlalo = 0;									// A szabályok száma
 var pakli_szamlalo = 52; 									// A pakli lapjainak száma
 var pakli = [												// A pakli mátrixa 
 	[1,2,3,4],
@@ -29,6 +29,7 @@ var pakli = [												// A pakli mátrixa
 	[45,46,47,48],
 	[49,50,51,52]
 ];
+var szabaly = new Array();
 
 // Random kártya kidobása
 function randomkartya(){
@@ -48,53 +49,59 @@ function randomkartya(){
 			pakli[randomnumber-1][randomchar-1] = 0;
 			pakli_szamlalo--;
 		
-		/* A kártya színének betűvé alakítása (Így fogja majd a képet beszúrni) */
-		switch(randomchar){
-		case 1:
-			randomchar = "c";
-			break;
-		case 2:
-			randomchar = "d";
-			break;
-		case 3:
-			randomchar = "h";
-			break;
-		case 4:
-			randomchar = "s";
-			break;
-		}
+			/* A kártya színének betűvé alakítása (Így fogja majd a képet beszúrni) */
+			switch(randomchar){
+			case 1:
+				randomchar = "c";
+				break;
+			case 2:
+				randomchar = "d";
+				break;
+			case 3:
+				randomchar = "h";
+				break;
+			case 4:
+				randomchar = "s";
+				break;
+			}
 
-		/* Kártyahúzás */
-		document.getElementById("lapok").innerHTML = "<img src='images/kartya/"+ randomnumber + randomchar +".gif'><br><br>Pakli: "+pakli_szamlalo+" lap";
+			/* Kártyahúzás */
+			document.getElementById("lapok").innerHTML = "<img src='images/kartya/"+ randomnumber + randomchar +".gif'><br><br>Pakli: "+pakli_szamlalo+" lap";
 
-		/* Ha dáma lett húzva bemegy az első IF-be, ott ellenőrzi, hogy volt-e már dáma húzva. 
-		   Ha volt, akkor az előző játékostol elveszi, és az új kapja meg.
-		   Ha nem volt, akkor a most soron lévő játékos simán megkapja. */
-		if (randomnumber == 12) {
-			if(dama == 1){
-				document.getElementById("dama" + tmp_n_dama + "").innerHTML = " ";
-				document.getElementById("dama" + n + "").innerHTML = " <img src='images/kartya/"+ randomnumber + randomchar +".gif'>";
-				tmp_n_dama = n;
-			} else {
-				document.getElementById("dama" + n + "").innerHTML = " <img src='images/kartya/"+ randomnumber + randomchar +".gif'>";
-				dama = 1;
-				tmp_n_dama = n;
+			/* Ha dáma lett húzva bemegy az első IF-be, ott ellenőrzi, hogy volt-e már dáma húzva. 
+			   Ha volt, akkor az előző játékostol elveszi, és az új kapja meg.
+			   Ha nem volt, akkor a most soron lévő játékos simán megkapja. */
+			if (randomnumber == 12) {
+				if(dama == 1){
+					document.getElementById("dama" + tmp_n_dama + "").innerHTML = " ";
+					document.getElementById("dama" + n + "").innerHTML = " <img src='images/kartya/"+ randomnumber + randomchar +".gif'>";
+					tmp_n_dama = n;
+				} else {
+					document.getElementById("dama" + n + "").innerHTML = " <img src='images/kartya/"+ randomnumber + randomchar +".gif'>";
+					dama = 1;
+					tmp_n_dama = n;
+				}
+			}
+
+			/* Az ásznál is ugyan az a módszer mint a dámánál. */
+			if (randomnumber == 1) {
+				if(asz == 1){
+					document.getElementById("asz" + tmp_n_asz + "").innerHTML = " ";
+					document.getElementById("asz" + n + "").innerHTML = " <img src='images/kartya/"+ randomnumber + randomchar +".gif'>";
+					tmp_n_asz = n;
+				} else {
+					document.getElementById("asz" + n + "").innerHTML = " <img src='images/kartya/"+ randomnumber + randomchar +".gif'>";
+					asz = 1;
+					tmp_n_asz = n;
+				}
+			}
+
+
+			if (randomnumber == 13) {
+					document.getElementById("szabaly_textbox").innerHTML = "<input type='text' id='ujszabaly'><button onclick='beir(); dobozreset();'>Beír</button>";
+					alert("\nKirályt húztál, új szabályt hozhatsz!\n");
 			}
 		}
-
-		/* Az ásznál is ugyan az a módszer mint a dámánál. */
-		if (randomnumber == 1) {
-			if(asz == 1){
-				document.getElementById("asz" + tmp_n_asz + "").innerHTML = " ";
-				document.getElementById("asz" + n + "").innerHTML = " <img src='images/kartya/"+ randomnumber + randomchar +".gif'>";
-				tmp_n_asz = n;
-			} else {
-				document.getElementById("asz" + n + "").innerHTML = " <img src='images/kartya/"+ randomnumber + randomchar +".gif'>";
-				asz = 1;
-				tmp_n_asz = n;
-			}
-		}
-	}
 	}
 }
 
@@ -102,6 +109,7 @@ function randomkartya(){
 function nullaz(){
 	korok=0; // Lenullázza a körök számát
 	pakli_szamlalo = 52; // A paklit újra feltölti
+
 	/* Reseteli a mátrixot */
 	for  (var h = 0; h <= 3; h++) {
 		for (var j = 0; j <= 12; j++) {
@@ -115,12 +123,17 @@ function nullaz(){
 	/* Reseteli az asztalt */
 	document.getElementById("lapok").innerHTML = "Nem volt még lap húzva!";
 	document.getElementById("kartyahuzas").innerHTML = '<a href="#" onclick="randomkartya(); soros();">Kártya húzás</a>';
+
+
+	for (var g = 0; g <= szabalyszamlalo; g++) {
+		torles(g);
+	}
 }
 
 //Új pakli
 function pluszpakli(){
 	korok = tmp_n + 1; // Megjegyzi kinél volt utoljára a sör, majd a következővel fogja folytatni
-	pakli_szamlalo = 52; // Feltölti a paklit
+	pakli_szamlalo = 52; // Feltölti a pakszabalyszamlaloit
 	/* Reseteli a mátrixot */
 	for  (var k = 0; k <= 3; k++) {
 		for (var j = 0; j <= 12; j++) {
@@ -153,4 +166,17 @@ function soros(){
 		document.getElementById("jatekos" + n + "").innerHTML = " <img src='images/beer-icon.png'> ";
 		korok++;
 	}
+}
+
+function beir(){
+	szabaly[szabalyszamlalo] = document.getElementById("ujszabaly").value;
+	document.getElementById("szabalyok").innerHTML += "<tr><td id='"+ szabalyszamlalo +"'>- " + szabaly[szabalyszamlalo] + " - <img src='images/x-red.gif' onclick='torles(" + szabalyszamlalo + ");'></td></tr>";
+	szabalyszamlalo++;
+}
+function torles(kod){
+	var torlesre = document.getElementById("" + kod + "");
+	torlesre.parentNode.removeChild(torlesre);
+}
+function dobozreset(){
+	document.getElementById("szabaly_textbox").innerHTML = "Ha királyt húzol, új szabályt írhatsz be";
 }
